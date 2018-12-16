19b942cc3bce9ba7746bb1cae94c4973bc935e16
Emiliano Sauvisky <esauvisky@gmail.com>
2018-12-16 16:12:18 -0200

    Patch for Calcy 2.62a and up

--- ivcheck.py
+++ ivcheck.py
@@ -32,7 +32,7 @@ formatter = ColoredFormatter("  %(log_color)s%(levelname)-8s%(reset)s | %(log_co
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-RE_CALCY_IV = re.compile(r"^./MainService\(\s*\d+\): Received values: Id: \d+ \((?P<name>.+)\), Nr: (?P<id>\d+), CP: (?P<cp>\-{0,1}\d+), Max HP: (?P<max_hp>\d+), Dust cost: (?P<dust_cost>\d+), Level: (?P<level>\-{0,1}[0-9\.]+), FastMove (?P<fast_move>.+), SpecialMove (?P<special_move>.+), Gender (?P<gender>\d), Level-up (true|false):$")
+RE_CALCY_IV = re.compile(r"^./MainService\(\s*\d+\): Received values: Id: \d+ \((?P<name>.+)\), Nr: (?P<id>\d+), CP: (?P<cp>\-{0,1}\d+), Max HP: (?P<max_hp>\d+), Dust cost: (?P<dust_cost>\d+), Level: (?P<level>\-{0,1}[0-9\.]+), FastMove (?P<fast_move>.+), SpecialMove (?P<special_move>.+),Gender (?P<gender>\d), catchYear (?P<catch_year>.+), Level-up (true|false):$")
 RE_RED_BAR = re.compile(r"^.+\(\s*\d+\): Screenshot #\d has red error box at the top of the screen$")
 RE_SUCCESS = re.compile(r"^.+\(\s*\d+\): calculateScanOutputData finished after \d+ms$")
 RE_SCAN_INVALID = re.compile(r"^.+\(\s*\d+\): Scan invalid$")
@@ -244,7 +244,7 @@ class Main:
         valid_conditions = [
             "name", "iv", "iv_min", "iv_max", "success", "blacklist",
             "appraised", "id", "cp", "max_hp", "dust_cost", "level",
-            "fast_move", "special_move", "gender"
+            "fast_move", "special_move", "gender", "catch_year"
         ]
         clipboard_required = ["iv", "iv_min", "iv_max"]
         for ruleset in self.config["actions"]:
